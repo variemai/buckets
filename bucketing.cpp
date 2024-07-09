d8c3823a37650b79bec7ba5a7c68da49056e6974
Ioannis Vardas <vardas@par.tuwien.ac.at>
2024-07-09 11:13:20 +0200

    use constexpr to optimize this

--- bucketing.cpp
+++ bucketing.cpp
@@ -4,40 +4,47 @@
 #include <vector>
 #include <chrono>
 #include <random>
-#define NUM_BUCKETS 10
 
-// Define thresholds as a std::array with constexpr
-constexpr std::array<int64_t, NUM_BUCKETS-1> thresholds = {8, 64, 512, 4096, 32768, 131072, 1048576, 8388608 ,33554432};
+constexpr size_t NUM_BUCKETS = 10;
+constexpr std::array<int64_t, NUM_BUCKETS - 1> thresholds = {8, 64, 512, 4096, 32768, 131072, 1048576, 8388608, 33554432};
+
+constexpr int get_bucket_index(int64_t value, size_t index = NUM_BUCKETS - 1) {
+    if (index == 0) {
+        return ~(value - thresholds[0] >> 63);  // Handle the first bucket explicitly
+    } else {
+        return ~(value - thresholds[index - 1] >> 63) + get_bucket_index(value, index - 1);
+    }
+}
 
 // Utility to generate masks and index calculation at compile time
-template <size_t N>
-struct BucketIndexHelper {
-    static int get_bucket_index(int64_t value) {
-        int64_t mask = ~((value - thresholds[N-1]) >> 63);
-        return mask + BucketIndexHelper<N-1>::get_bucket_index(value);
-    }
-};
-
-template <>
-struct BucketIndexHelper<1> {
-    static int get_bucket_index(int64_t value) {
-        int64_t mask = ~((value - thresholds[0]) >> 63);
-        return mask;
-    }
-};
-
-template <>
-struct BucketIndexHelper<0> {
-    static int get_bucket_index(int64_t) {
-        return 0;
-    }
-};
-
-template <size_t N>
-int get_bucket_index(int64_t value) {
-    //static_assert(N <= thresholds.size(), "N must not exceed the size of thresholds");
-    return BucketIndexHelper<N>::get_bucket_index(value);
-}
+//template <size_t N>
+//struct BucketIndexHelper {
+//    static int get_bucket_index(int64_t value) {
+//        int64_t mask = ~((value - thresholds[N-1]) >> 63);
+//        return mask + BucketIndexHelper<N-1>::get_bucket_index(value);
+//    }
+//};
+//
+//template <>
+//struct BucketIndexHelper<1> {
+//    static int get_bucket_index(int64_t value) {
+//        int64_t mask = ~((value - thresholds[0]) >> 63);
+//        return mask;
+//    }
+//};
+//
+//template <>
+//struct BucketIndexHelper<0> {
+//    static int get_bucket_index(int64_t) {
+//        return 0;
+//    }
+//};
+//
+//template <size_t N>
+//int get_bucket_index(int64_t value) {
+//    //static_assert(N <= thresholds.size(), "N must not exceed the size of thresholds");
+//    return BucketIndexHelper<N>::get_bucket_index(value);
+//}
 
 int
 choose_bucket(int64_t bytes) {
@@ -118,7 +125,7 @@ void benchmark() {
     // Measure the time taken to get the bucket index for each value
     start = std::chrono::high_resolution_clock::now();
     for (const auto& value : values) {
-        bucket = get_bucket_index<thresholds.size()>(value);
+        bucket = get_bucket_index(value);
         //indices1.push_back(bucket);
     }
     end = std::chrono::high_resolution_clock::now();
